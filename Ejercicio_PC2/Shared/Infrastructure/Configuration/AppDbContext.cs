a8b243a1c9a658307bbcd71b0d6dad089acf8b58
José Anthony Arenas Conde <142860250+JoseACd744@users.noreply.github.com>
2023-11-07 19:25:56 -0500

    addded bounded context items

--- Ejercicio_PC2/Shared/Infrastructure/Configuration/AppDbContext.cs
+++ Ejercicio_PC2/Shared/Infrastructure/Configuration/AppDbContext.cs
@@ -1,6 +1,26 @@
-﻿namespace Ejercicio_PC2.Shared.Infrastructure.Configuration;
+﻿using Ejercicio_PC2.Shared.Infrastructure.Configuration.Extensions;
+using Ejercicio_PC2.Subscriptions.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace Ejercicio_PC2.Shared.Infrastructure.Configuration;
+
+public class AppDbContext : DbContext
 {
+    public DbSet<Destination> Destinations { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Destination>().HasKey(x => x.name);
+        modelBuilder.Entity<Destination>().Property(x => x.name).HasColumnName("name");
+        modelBuilder.Entity<Destination>().Property(x => x.MaxUsers).HasColumnName("max_users");
+        modelBuilder.Entity<Destination>().Property(x => x.IsRisky).HasColumnName("is_risky");
+        
+        modelBuilder.UseSnakeCaseNamingConvention();
+    }
     
 }
---
+++ Ejercicio_PC2/Subscriptions/Domain/Models/Entities/Destination.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio_PC2.Subscriptions.Domain.Models.Entities;
+
+public class Destination
+{
+    public string name { get; set; }
+    public int MaxUsers { get; set; }
+    public int IsRisky { get; set; }
+}
