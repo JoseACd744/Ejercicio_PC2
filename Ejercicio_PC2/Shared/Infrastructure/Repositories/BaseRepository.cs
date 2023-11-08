66d0462f55ad5fdf17ea206c2d1781e1dd87a5f0
José Anthony Arenas Conde <142860250+JoseACd744@users.noreply.github.com>
2023-11-07 19:09:08 -0500

    BaseRepo added

--- Ejercicio_PC2/Shared/Infrastructure/Repositories/BaseRepository.cs
+++ Ejercicio_PC2/Shared/Infrastructure/Repositories/BaseRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Ejercicio_PC2.Shared.Infrastructure.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ejercicio_PC2.Shared.Infrastructure.Repositories;
 
 public class BaseRepository
 {
-    
+    protected readonly DbContext _context;
+
+    public BaseRepository(DbContext context)
+    {
+        _context = context;
+    }
 }
